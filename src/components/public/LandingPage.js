21d4034f00e4d874c3ddf6ad8fd3026cd3869940
James Star <jamess.developer@gmail.com>
2025-02-16 23:13:25 +1300

    refactor: further tweaks

--- src/components/public/LandingPage.js
+++ src/components/public/LandingPage.js
@@ -53,8 +53,8 @@ const LandingPage = () => {
       'Wisdom', 'Prayer', 'God', 'Jesus'
     ];
     return tagEl.map((tag, index) => {
-      return <button key={`tag${index}`} className="mb-1 sm:mb-4">
-        <span key={tag} className="border border-gray-800 text-secondary bg-transparent text-base font-sans font-extralight me-3 sm:me-4 px-5 py-2.5 rounded">
+      return <button key={`tag${index}`} className="mb-1 sm:mb-3.5">
+        <span key={tag} className="border border-gray-800 text-secondary bg-white theme-dark:bg-transparent text-base font-sans font-light me-3 sm:me-4 px-5 py-2.5 rounded">
           {tag}
         </span>
       </button>
@@ -169,15 +169,6 @@ const LandingPage = () => {
               {renderSocialMediaIcons()}
             </div>
           </div>
-          <div>
-            <a href="https://github.com/agilitycycle/Diffrently" target="_blank">
-              <div className="mx-auto mb-16 w-[85px]">
-                <svg width="98" height="96" xmlns="http://www.w3.org/2000/svg">
-                  <path className="opacity-85 fill-secondary theme-dark:fill-gray-400" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"/>
-                </svg>
-              </div>
-            </a>
-          </div>
           <div className="mb-10 text-center text-base text-secondary theme-dark:text-gray-400 font-sans font-extralight leading-relaxed">
             Want to use Diffrently. commercially, PM james@agilitycycle.com to purchase a one-time commercial license.
           </div>
@@ -188,11 +179,11 @@ const LandingPage = () => {
             Pricing
           </div>
           <div className="mb-8 lg:mb-16">
-            <div className="w-full md:w-1/2 block mx-auto border border-gray-800 bg-transparent text-secondary theme-dark:text-gray-400 font-sans font-extralight">
+            <div className="w-full md:w-2/4 block mx-auto border border-gray-800 bg-white theme-dark:bg-transparent text-secondary theme-dark:text-gray-400 font-sans font-extralight">
               <div className="pt-10 pl-9 pr-9 text-center">
-                <div className="mb-3.5 text-3xl font-medium">Author</div>
+                <div className="mb-3.5 text-5xl font-medium">Author</div>
                 <div className="mb-3.5 text-base">Write a book every 5 mins with our AI co-pilot.</div>
-                <div className="mb-3.5 text-2xl font-medium">$25 USD</div>
+                <div className="mb-6 text-3xl font-light">$25 USD</div>
                 <button type="button" onClick={() => navigate('/signin')} className="block mx-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium text-base px-8 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                   Sign up
                 </button>
